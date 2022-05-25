95e17d0536cfe6ea9e8c484a0f755686ee8dc76a
chickenislife6 <tylerliuyang@gmail.com>
2022-05-24 20:00:26 -0400

    now with events

--- get_all_pools.py
+++ get_all_pools.py
@@ -1,37 +1,18 @@
-import codecs
-from gql import gql, Client
-from gql.transport.aiohttp import AIOHTTPTransport
-transport = AIOHTTPTransport(url="https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3")
-client = Client(transport=transport, fetch_schema_from_transport=True)
-query = gql("""
-query UNISWAP($skip: Int, $first: Int, $txLimit: BigInt){
-  pools(first: $first, skip: $skip, where: {txCount_gt: $txLimit}) {
-    token0 {
-        id
-        name
-    }
-    token1 {
-        id
-        name
-    }
-    txCount
-    id
-    
-  }
-}
-""")
-def main():
-    params = {"skip": -1000, "first": 1000, "txLimit" : 100}
-    result = {}
-    with codecs.open("pools.txt", "w") as file: 
+from web3 import HTTPProvider, Web3
+from web3.contract import Contract
+from web3._utils.filters import LogFilter
+from env import INFURAHTTPS
 
-        while result.get("pools") != []:
-            params["skip"] += 1000
-            result = client.execute(query, variable_values=params)
-
-            for i in result["pools"]:
-                file.write(i["token0"]["id"] + " " + ''.join(letter for letter in i["token0"]["name"] if letter.isalnum()) + " ")
-                file.write(i["token1"]["id"] + " " + ''.join(letter for letter in i["token1"]["name"] if letter.isalnum()) + " ")
-                file.write(i["id"] + " ")
-                file.write("\n")
-main()
+w3 = Web3(HTTPProvider(INFURAHTTPS))
+a = open("UniswapV3abi.json", 'r')
+address = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+abi = a.read()
+contract_instance: Contract = w3.eth.contract(address=address, abi=abi)
+Pool_filter: LogFilter = contract_instance.events.PoolCreated.createFilter(fromBlock="0x0")
+all_pool_creation_events = Pool_filter.get_all_entries()
+with open("pools.txt", "w") as file: 
+    for i in all_pool_creation_events:
+        file.write(i['args']["token0"] + " ")
+        file.write(i['args']["token1"] + " ")
+        file.write(i['args']['pool'] + " ")
+        file.write("\n")
